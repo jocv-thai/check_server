1e78065dd30b292ab7ef84a5e5cad0dc6988c5db
Your Name <you@example.com>
2020-07-01 17:16:54 +0900

    wait for 3 sec

--- cypress/integration/check_basic.js
+++ cypress/integration/check_basic.js
@@ -4,6 +4,9 @@ describe('Check Basic', function() {
         cy.visit(Cypress.env('CHECK_URL'))
         // check title
         cy.title().should(Cypress.env('CHECK_TITLE'))
+
+        // wait for 3 sec
+        cy.wait(3000)
         // check css element
         cy.get(Cypress.env('CHECK_CSS_ELEMENT')).should('exist')
     })
